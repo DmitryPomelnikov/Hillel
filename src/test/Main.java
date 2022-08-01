eabdc77c5c074e198bdb9d312e052514e5699e2b
Dmitry <pomelnikovda@gmail.com>
2022-08-01 22:47:11 +0300

    commit gitignore

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,10 +1,12 @@
 package test;
-
 public class Main {
     public static void main(String[] args) {
-        int a=1;
-        for (int i = 0; i<10; i++){
-            System.out.println(a+". Dmitry Pomelnikov");
+        int a = 1;
+        while (a <= 10) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.print(a + "*" + i + "=" + (a * i) + " ");
+            }
+            System.out.println("");
             a++;
         }
     }
